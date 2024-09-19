f20a0d1c14f056609bf70a841567310976f8e98b
gabrielcasag <gaspericasag@gmail.com>
2024-09-19 01:20:18 -0300

    add crxjs/vite-plugin tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
